3bce0f563bf0a4036423bb181e3bf94a36c4dfb2
Bruna <bruna.colomer@estudiantat.upc.edu>
2025-09-14 09:42:00 +0200

    new libraries

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,19 @@
 //#include "Peer-server.hpp"
 #include "Script.hpp"
 #include "Network.hpp"
+#include <nlohmann/json.hpp>
+#include <httplib.h>
+
+void server() {
+    httplib::Server svr;
+    std::cout <<"hola"<< std::endl;
+        svr.Get("/hi", [](const httplib::Request&, httplib::Response& res) {
+        nlohmann::json j = {{"msg", "Hello World!"}};
+        res.set_content(j.dump(), "application/json");   // IMPORTANT: content-type
+    });
+
+    svr.listen("0.0.0.0", 8080);
+}
 
 int main() {
     //boost::asio::io_context io;
@@ -13,6 +26,9 @@ int main() {
 
     //Script script("job.lua");
     //script.run();
+    server();
+    
+    //std::thread t(server);
 
     Network net;
     return 0;
